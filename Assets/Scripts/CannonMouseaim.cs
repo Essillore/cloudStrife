c4e35f393d48e26eb850ac421210775dd28224c0
Essillore <riikka.pehkonen@me.com>
2023-03-28 18:13:32 +0300

    Commit 2803 Mouseaim
    Raycasted CannonMouseaim in use. Still broken (probably because of camera orbit script)

---
+++ Assets/Scripts/CannonMouseaim.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonMouseaim : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    public float projectileSpeed = 1000f;
+    public Camera myCamera;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = myCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                transform.LookAt(hit.point);
+            }
+            GameObject projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody>().AddForce(transform.forward * projectileSpeed);
+
+            /*if (hit.transform.CompareTag("Enemy"))
+            {
+                Destroy(hit.transform.gameObject);
+            }
+            */
+        }
+
+    }
+
+}
--- Assets/Scripts/MouseScriptAether.cs
+++ Assets/Scripts/MouseScriptAether.cs
@@ -5,6 +5,26 @@ using UnityEngine;
 public class MouseScriptAether : MonoBehaviour
 {
 
+    private void FixedUpdate()
+    {
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
+        if (Physics.Raycast(myRay, out hit, Mathf.Infinity))
+        {
+            //  transform.position = hit.point;
+
+            transform.position = Vector3.Lerp(transform.position, hit.point, 0.1f);
+
+            if (Input.GetButtonDown("Fire1") && hit.transform.CompareTag("Enemy"))
+            {
+                Destroy(hit.transform.gameObject);
+            }
+
+
+        }
+
+
+    }
 
 }
