587425db54edb355a4e1c8091596bb38fa10a43c
Essillore <riikka.pehkonen@me.com>
2023-03-30 16:29:18 +0300

    Esc quit ingame

--- Assets/Scripts/Puiseva/HealingRecharge.cs
+++ Assets/Scripts/Puiseva/HealingRecharge.cs
@@ -7,25 +7,26 @@ public class HealingRecharge : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
-            {
+        {
             GetComponentInParent<Spire>().healing = true;
         }
 
     }
     private void OnTriggerExit(Collider collision)
-    { if (collision.CompareTag("Player"))
+    {
+        if (collision.CompareTag("Player"))
         { GetComponentInParent<Spire>().healing = false; }
     }
 }
