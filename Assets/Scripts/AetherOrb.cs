e27f8c8b6b2fa5c7fb20160db850e5a043d79b0d
Essillore <riikka.pehkonen@me.com>
2023-03-31 09:37:27 +0300

    Audio to pickup

--- Assets/Scripts/AetherOrb.cs
+++ Assets/Scripts/AetherOrb.cs
@@ -6,6 +6,11 @@ public class AetherOrb : MonoBehaviour
 {
     public int healAmount = 50;
 
+    public AudioClip pickUp;
+    AudioSource audioSource;
+
+   
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +30,8 @@ public class AetherOrb : MonoBehaviour
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
             if (other.GetComponent<PlayerHealth>().currentHealth > 0)
             {
+                audioSource.PlayOneShot(pickUp, 0.5F);
+
                 playerHealth.Heal(healAmount);
                 Destroy(transform.root.gameObject);
             }
